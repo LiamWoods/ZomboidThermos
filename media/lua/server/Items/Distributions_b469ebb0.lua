890a029983b68b17283ba70eec25896afb5cc675
Liam <woodsie123@hotmail.co.uk>
2020-12-06 21:53:08 +0000

    Adjust values - incorrect delta assumptions

--- media/lua/server/Items/Distributions_b469ebb0.lua
+++ media/lua/server/Items/Distributions_b469ebb0.lua
@@ -2,17 +2,6 @@ Distributions = Distributions or {};
 
 local distributionTable = 
 {
-	kitchen =
-		{
-			counter = {
-				rolls = 3,
-				items = {
-					"ThermoMug",100,
-					"WaterThermoMug",100,
-					"ThermoMugCuppa",100,
-				}
-			}
-		},
 }
 
 table.insert(Distributions, distributionTable);
