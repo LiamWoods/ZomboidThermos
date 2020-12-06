eb6ad58df0e1532b0709194a4ea6b84b8ed86495
Liam <woodsie123@hotmail.co.uk>
2020-12-06 22:14:38 +0000

    Re-add dist

--- media/lua/server/Items/Distributions_b469ebb0.lua
+++ media/lua/server/Items/Distributions_b469ebb0.lua
@@ -2,6 +2,17 @@ Distributions = Distributions or {};
 
 local distributionTable = 
 {
+	kitchen =
+		{
+			counter = {
+				rolls = 3,
+				items = {
+					"ThermoMug",100,
+					"WaterThermoMug",100,
+					"ThermoMugCuppa",100,
+				}
+			}
+		},
 }
 
 table.insert(Distributions, distributionTable);
